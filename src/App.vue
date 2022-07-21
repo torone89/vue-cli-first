0aaf6672198380169f76a3a9014aa72835896140
torone89 <torone89>
2022-07-21 15:08:17 +0200

    Esercizio completo + bonus no stile img

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <MyTitle />
+
+      <MyHero />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyTitle from './components/Title.vue'
+import MyHero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  MyTitle,
+  MyHero
   }
 }
 </script>
---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>"La mia prima app con Vue CLI!"</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTitle',
+  
+}
+</script>
+
+<style>
+
+</style>
