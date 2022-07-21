7b5f5d545602d6ff38d167adfc1ff718413c1d5e
torone89 <torone89>
2022-07-21 15:08:00 +0200

    Esercio complete + Bonus no stile img

---
+++ src/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hero-sytle">
+ <img src="../assets/returnal.jpg" alt="">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MYHero',
+}
+</script>
+
+<style>
+.hero-sytle {height: 100px}
+</style>
